5a8ff72032e6a4a9bb08449a09ab9e1a6968c9d9
Tai Nguyen <tainguyen880s@gmail.com>
2023-10-11 11:01:34 +0700

    edit home page pc

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,19 @@
 import clsx from "clsx";
 import style from "./Home.module.scss";
+import HeaderMain from "~/layouts/MainLayout/HeaderMain";
+import Functional from "~/components/Functional"
+import ManageSolution from "~/components/ManageSolution"
+import Package from "~/components/Package"
+import TeamContact from "~/components/TeamContact"
 const Home = () => {
-  return <div className={clsx(style.homeWapper)}></div>;
+  return <div className={clsx(style.homeWapper)}>
+    <HeaderMain />
+    <Functional />
+    <ManageSolution />
+    <Package />
+    <TeamContact />
+    
+  </div>;
 };
 
 export default Home;
